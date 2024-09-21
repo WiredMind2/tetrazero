22ff3f15a9a893ba11c1c66dcce72f61bba0f125
WiredMind2 <william@michaud.re>
2024-09-21 22:40:24 +0200

    bugfix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+	async redirects() {
+		return [
+		  // Basic redirect
+		  {
+			source: '/index.html',
+			destination: '/',
+			permanent: true,
+		  },
+		]
+	  },
 };
 
 export default nextConfig;
--- scripts/source_fetcher.py
+++ scripts/source_fetcher.py
@@ -21,6 +21,7 @@ for url, folder in urls.items():
     text_replace = {
         "class=": "className=",  # JSX fix
         "<img ": "<Image ",  # Next.js Image component
+        '<div className="w-icon-nav-menu"></div>': '<svg className="w-icon-nav-menu" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>'
     }
 
     for src, dst in text_replace.items():
